5ff382388427fbe25b07822010ee353bd5cd05c6
Angyee Alexandra <78864768+Angxandralol@users.noreply.github.com>
2023-08-13 02:17:40 -0400

    feature[PRO-57]: Creación del componente del footer

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
 const routes: Routes = [
   {path: "", component: LoginComponent}, 
-  {path: "navbar", component: NavbarComponent}
 ];
 
 @NgModule({
