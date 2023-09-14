7c11b3094545f4a0046bbd53413134dcc7df1d0f
Angyee Alexandra <78864768+Angxandralol@users.noreply.github.com>
2023-09-13 20:35:22 -0400

    feature[PRO-34]: Creación de la vista principal del historial

---
+++ app/src/app/components/historial/historial-inicio/historial-inicio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-inicio',
+  templateUrl: './historial-inicio.component.html',
+  styleUrls: ['./historial-inicio.component.scss']
+})
+export class HistorialInicioComponent {
+
+}
