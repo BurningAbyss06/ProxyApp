3d29d9a68d5008247385f8382d184188bf714724
Angyee Alexandra <78864768+Angxandralol@users.noreply.github.com>
2023-09-15 20:25:00 -0400

    feature[PRO-35]: Creación de la vista de detalles de historial

--- app/src/app/modules/historial/historial.module.ts
+++ app/src/app/modules/historial/historial.module.ts
@@ -3,16 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { HistorialInicioComponent } from 'src/app/components/historial/historial-inicio/historial-inicio.component';
+import { DetallesComponent } from 'src/app/components/historial/detalles/detalles.component';
 import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
     HistorialInicioComponent, 
+    DetallesComponent,
     FooterComponent
   ],
   imports: [
     CommonModule
+  ], 
+  exports: [
+    HistorialInicioComponent,
+    DetallesComponent
   ]
 })
 export class HistorialModule { }
